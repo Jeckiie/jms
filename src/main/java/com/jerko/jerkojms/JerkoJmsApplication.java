59e815e90ef2ac4fada79348a41938af76d36ef8
jerkokosic <jerkokosic1@gmail.com>
2021-04-29 15:19:06 +0200

    initial

---
+++ src/main/java/com/jerko/jerkojms/JerkoJmsApplication.java
@@ -0,0 +1,13 @@
+package com.jerko.jerkojms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerkoJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JerkoJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jerko/jerkojms/JerkoJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerko.jerkojms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerkoJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
